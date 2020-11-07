0b085aeae7c33ee0e977984d56b65a8aa867884f
Bailey McKelway <bjlmckelway@gmail.com>
2020-11-06 20:41:58 -0500

    Update scrapeClass.py

--- scrapeClass.py
+++ scrapeClass.py
@@ -9,11 +9,6 @@ from datetime import datetime
 from datetime import datetime
 from fuzzywuzzy import fuzz
 
-key = 'NSkKvBJI7UHeUdWAlvsYknQEm'
-secret = 'oBD3k0uczP5nkTcVnNkmKkuF0tel4vtAzYEHQHSj7gHRdmP1kG'
-
-token = '1321211489278173186-9GrEzJ3CT1XKEhS1wEav2FiFBAVCKj'
-tokenSecret = '2cqLHWi9vE6hDeLOKobZFDD3h5NnUIR24biOCTxDVbLAu'
 
 auth = tweepy.OAuthHandler(key, secret)
 auth.set_access_token(token, tokenSecret)
