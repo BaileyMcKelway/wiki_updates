b433829d177a7b9f21dd19f5b954d4c464bcc114
Bailey McKelway <bjlmckelway@gmail.com>
2020-11-09 16:08:15 -0500

    Update scrapeClass.py

--- scrapeClass.py
+++ scrapeClass.py
@@ -372,11 +372,6 @@ class WikiUpdate:
         return changedText
 
 
-key = 'NSkKvBJI7UHeUdWAlvsYknQEm'
-secret = 'oBD3k0uczP5nkTcVnNkmKkuF0tel4vtAzYEHQHSj7gHRdmP1kG'
-
-token = '1321211489278173186-9GrEzJ3CT1XKEhS1wEav2FiFBAVCKj'
-tokenSecret = '2cqLHWi9vE6hDeLOKobZFDD3h5NnUIR24biOCTxDVbLAu'
 
 
 wikiTitles = ['National_responses_to_the_COVID-19_pandemic',
