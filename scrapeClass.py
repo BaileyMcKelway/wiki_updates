3bffe5aedf3387a657116807edf3b668cfe02eb7
Bailey McKelway <bjlmckelway@gmail.com>
2020-11-11 13:52:00 -0500

    Update scrapeClass.py

--- scrapeClass.py
+++ scrapeClass.py
@@ -361,41 +361,4 @@ class WikiUpdate:
             return changedText.replace(search, name)
 
 
-key = 'NSkKvBJI7UHeUdWAlvsYknQEm'
-secret = 'oBD3k0uczP5nkTcVnNkmKkuF0tel4vtAzYEHQHSj7gHRdmP1kG'
 
-token = '1321211489278173186-9GrEzJ3CT1XKEhS1wEav2FiFBAVCKj'
-tokenSecret = '2cqLHWi9vE6hDeLOKobZFDD3h5NnUIR24biOCTxDVbLAu'
-
-
-wikiTitles = ['National_responses_to_the_COVID-19_pandemic',
-              'COVID-19_vaccine',
-              'Workplace_hazard_controls_for_COVID-19',
-              'COVID-19_pandemic_in_the_United_States',
-              'COVID-19_pandemic_in_Brazil',
-              'COVID-19_pandemic_in_India',
-              'COVID-19_pandemic_in_Russia',
-              'COVID-19_pandemic_in_South_Africa',
-              'COVID-19_pandemic_in_Mexico',
-              'COVID-19_pandemic_in_Chile',
-              'COVID-19_pandemic_in_the_United_Kingdom',
-              'COVID-19_pandemic_in_Iran',
-              'Severe_acute_respiratory_syndrome_coronavirus_2',
-              'Coronavirus',
-              'COVID-19_pandemic',
-              'Coronavirus_disease_2019',
-              'Coronavirus_disease',
-              'COVID-19_recession',
-              'Misinformation_related_to_the_COVID-19_pandemic',
-              'COVID-19_drug_development',
-              'Impact_of_the_COVID-19_pandemic_on_science_and_technology',
-              'Impact_of_the_COVID-19_pandemic_on_the_environment',
-              'Impact_of_the_COVID-19_pandemic_on_politics',
-              'Financial_market_impact_of_the_COVID-19_pandemic',
-              'Impact_of_the_COVID-19_pandemic_on_social_media',
-              'Mental_health_during_the_COVID-19_pandemic',
-              'Human_rights_issues_related_to_the_COVID-19_pandemic']
-
-Scraper = WikiUpdate(wikiTitles, key, secret, token, tokenSecret)
-
-Scraper.mainFunc()
